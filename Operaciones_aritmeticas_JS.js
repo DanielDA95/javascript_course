6d8ee1d9bbc470bafa26c98cc9ce74f6cdf5dcff
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-25 12:27:42 +0200

    Operaciones_aritmeticas_JS.js

--- Operaciones_aritmeticas_JS.js
+++ Operaciones_aritmeticas_JS.js
@@ -1,3 +1,18 @@
+/* Order of Operations
+   PEMDAS -> PEDMAS
+   Please Excuse My Dear Aunt Sally
+   Paranthesis
+   Exponents
+   Multiplication 5 / 5 * 4
+   Division
+   Addition
+   Subtraction
+*/
+
+var num = 5 + (5 * 10) / 6**2 - 1;
+var num = 5 / 5 * 4;
+
+
 2 + 2; // 4
 
 2 - 2; // 0
