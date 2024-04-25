932a4d8545ff9721fd2cab91f0416b8aeb220600
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-25 12:21:57 +0200

    Operaciones_aritmeticas_JS.js

--- Operaciones_aritmeticas_JS.js
+++ Operaciones_aritmeticas_JS.js
@@ -43,3 +43,23 @@ var strNum = '100';
 var convertedNum = + strNum;
 
 convertedNum; // 100
+
+
+//______________________________________________________________________________________________________________________
+//Asignacion de operadores compuestos
+
+var name = 'Tiffany';
+
+var sum = 0;
+
+var gradeOne = 100;
+
+var gradeTwo = 80;
+
+sum += gradeOne; // 100
+
+sum; // 100
+
+sum += gradeTwo; // 180
+
+sum *= gradeTwo; // 14400
