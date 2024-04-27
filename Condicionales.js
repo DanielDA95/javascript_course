cacf90370112157b24df046951266362c75b3089
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-27 13:03:15 +0200

    Update Condicionales.js

--- Condicionales.js
+++ Condicionales.js
@@ -23,5 +23,37 @@ if (age <= 10) {
 }
 
 //________________________________________________________________________________________________________________________________________________________________--
+// Condicionales if-else if-else if
 
+var age = 30;
 
+if (age <= 10) {
+  console.log("You can eat from the kid's menu");
+  console.log("You are not old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 16 && age < 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are not old enough to rent a car");
+} else if (age >= 25) {
+  console.log("You can not eat from the kid's menu");
+  console.log("You are old enough to drive");
+  console.log("You are old enough to rent a car");
+}
+//___________________________________________________________________________________________________________________________________
+//Cómo crear una instrucción switch en JavaScript para comprobar los tipos de datos (DECLARACION DE UN CASO)
+var dataPoint = {};
+
+switch (typeof dataPoint) {
+  case "string":
+    console.log("It's a string");
+    break;
+  case "number":
+    console.log("It's a number");
+    break;
+  case "boolean":
+    console.log("It's a boolean");
+    break;
+  default:
+    console.log('No matches');
+}
