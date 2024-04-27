0e873cd3b698bbb26d23e2795b787720888ee860
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-27 16:17:55 +0200

    Create Funciones.js

---
+++ Funciones.js
@@ -0,0 +1,21 @@
+// Funciones con su sintaxis basica. 
+
+function hiThere () {
+    console.log('Hi there');
+}
+
+hiThere(); // Hi there
+
+function hiThereTwo() {
+    return 'Hi there again';
+}
+
+hiThereTwo(); // "Hi there again"
+
+var storedText = hiThere(); // Hi there
+
+storedText; // undefined
+
+var storedTextTwo = hiThereTwo();
+
+storedTextTwo; // "Hi there again"
