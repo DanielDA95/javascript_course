9470c7ccab6ee9ddc2ce4119ba2c470d5486eb6e
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-27 12:06:34 +0200

    Create condicionales.js

---
+++ condicionales.js
@@ -0,0 +1,23 @@
+//Lo siguiente es la sintaxis basica de las condicionales.
+var edad = 30;
+var edad_dos = 29;
+
+/* Forma de saber si es igual 
+if (edad === edad_dos) {
+  console.log('They are equal')
+}
+*/
+
+//Forma de saber si NO es igual
+if ( edad !== edad_dos) {
+  console.log('Not equal')
+}
+
+// Mayor o igual que
+if (age >= 25) {
+  console.log('Old enough to rent a car');
+}
+// Menor o igual que
+if (age <= 10) {
+  console.log('You can eat from the kid menu');
+}
