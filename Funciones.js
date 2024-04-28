374060c9506e4f4b947657d8211034d44ff2c056
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-28 11:41:47 +0200

    Update Funciones.js

--- Funciones.js
+++ Funciones.js
@@ -19,3 +19,22 @@ storedText; // undefined
 var storedTextTwo = hiThereTwo();
 
 storedTextTwo; // "Hi there again"
+
+
+//_______________________________________________________________________________________________
+//Como funciona el ambito variable en JS
+var userObj = {
+  email: 'sample@example.com',
+  fullName: 'Kristine Hudgens'
+}
+
+function dashboardGreeting() {
+  var userObj = {
+    email: 'sample2@example.com',
+    fullName: 'Jordan Hudgens'
+  }
+  console.log("Hi there, ".concat(userObj.fullName));
+}
+
+dashboardGreeting();
+console.log(userObj.fullName);
