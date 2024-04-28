8ecd0c5dd02c49e55e9e38f51ec086a37672a3de
DanielDA95 <126833141+DanielDA95@users.noreply.github.com>
2024-04-28 14:58:38 +0200

    Tipos_de_funciones.js

--- Tiposde_funciones_JS.js
+++ Tiposde_funciones_JS.js
@@ -98,3 +98,54 @@ var str2 = str.concat('again and again');
 
 str2.indexOf('again'); // 44
 str2.lastIndexOf('again'); // 54
+
+//________________________________________________________________________________________________________________________________________________________
+//Como trabajar con argumentos de función. 
+function nombreCompleto(primernombre, apellido) {
+  return apellido.toUpperCase() + ", " + primernombre.toUpperCase();
+}
+
+nombreCompleto('Carolina', 'Agudelo')
+
+//_______________________________________________________________Otro ejemplo
+function nombreCompleto(primernombre, apellido, languaje) {
+  var languaje = languaje || 'Español';
+  return apellido.toUpperCase() + ", " + primernombre.toUpperCase() + ' - ' + languaje;
+}
+
+console.log(nombreCompleto('Carolina', 'Agudelo'));
+
+//____________________________________________________________________________________________________________________________________________________________
+//var someUser = {
+    name: 'Jordan'
+}
+
+function nameFormatter (user) {
+    return user.name = 'Oops';
+}
+
+nameFormatter(someUser); // "Oops"
+
+someUser; // Object {name: "Oops"}
+
+var someName = 'Tiffany';
+
+function someOtherNameFormatter(name) {
+    return name = 'Oops';
+}
+
+someOtherNameFormatter(someName); // "Oops"
+
+someName; // "Tiffany"
+
+function nameFormatter (userName) {
+    return userName = 'Oops';
+}
+
+someUser.name; // "Oops"
+
+someUser.name = 'Kristine';
+
+nameFormatter(someUser.name); // "Oops"
+
+someUser.name; // "Kristine"
